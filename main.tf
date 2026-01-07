518d5fcbf182db9f99f95ac60365a54d8bb5ceb3
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-01-07 05:44:40 +0530

    terraform docker

---
+++ main.tf
@@ -0,0 +1,46 @@
+resource "aws_instance" "instance" {
+  for_each               = var.components
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.vpc_security_group_ids
+  volume_tags = {
+    volume_size = lookup(each.value,root_volume_size)
+  }
+
+  tags = {
+      Name = each.key
+  }
+}
+
+resource "aws_route53_record" "dns_records" {
+    for_each = var.components
+    zone_id  = var.zone_id
+    name     = "${each.key}-dev"
+    type     = "A"
+    ttl      = 5
+    records  = [aws_instance.instance[each.key].private_ip]
+}
+
+
+resource "null_resource" "ansible" {
+    depends_on = [
+        aws_instance.instance,
+        aws_route53_record.dns_records
+    ]
+
+    for_each   = var.components
+
+    provisioner "remote-exec" {
+        connection {
+            type      = "ssh"
+            user      = "ec2-user"
+            password  = "DevOps321"
+            host      = aws_instance.instance[each.key].private_ip
+        }
+
+        inline = [
+            "ansible-pull -i localhost, -U https://github.com/Sandeepkumar0088/roboshop-ansible-docker.git main.yml -e component=${each.key} -e env=dev"
+        ]
+
+    }
+}
---
+++ variables.tf
@@ -0,0 +1,60 @@
+variable "ami"{
+    default = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_type"{
+    default = "t3.micro"
+}
+
+variable "vpc_security_group_ids" {
+    default = [ "sg-039df2e97e51ebc17" ]
+}
+
+
+variable "zone_id" {
+    default = "Z09354891N46GVLJSDZH0"
+}
+
+variable "components" {
+    default = {
+      frontend  = { ports = { ssh = 22, frontend = 80 }
+        instance_type  = "t3.micro"
+        root_volume_size = 25
+      }
+      mongodb   = { ports = { ssh = 22, mongodb = 27017 }
+        instance_type  = "t3.micro"
+      }
+      catalogue = { ports = { ssh = 22, app = 8080 }
+        instance_type  = "t3.micro"
+        root_volume_size = 25
+      }
+      redis     = { ports = { ssh = 22, redis = 6379 }
+        instance_type  = "t3.micro"
+      }
+      user      = { ports = { ssh = 22, app = 8080 }
+        instance_type  = "t3.micro"
+        root_volume_size = 25
+      }
+      cart      = { ports = { ssh = 22, app = 8080 }
+        instance_type  = "t3.micro"
+        root_volume_size = 25
+      }
+      mysql     = { ports = { ssh = 22, mysql = 3306 }
+        instance_type  = "t3.micro"
+      }
+      shipping  = { ports = { ssh = 22, app = 8080 }
+        instance_type  = "t3.micro"
+        root_volume_size = 25
+      }
+      rabbitmq  = { ports = { ssh = 22, rabbitmq = 5672 }
+        instance_type  = "t3.micro"
+      }
+      payment   = { ports = { ssh = 22, app = 8080 }
+        instance_type  = "t3.micro"
+        root_volume_size = 25
+      }
+      dispatch  = { ports = { ssh = 22, app = 8080 }
+        instance_type  = "t3.micro"
+      }
+    }
+}
