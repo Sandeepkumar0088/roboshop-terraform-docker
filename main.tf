bc468edcb6b4ca88fae6a51489d77e61a4b662b3
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-01-07 07:13:53 +0530

    terraform docker

--- main.tf
+++ main.tf
@@ -18,29 +18,33 @@ resource "aws_route53_record" "dns_records" {
   zone_id  = var.zone_id
   name     = "${each.key}-dev"
   type     = "A"
-  ttl      = 5
+  ttl      = 10
   records  = [aws_instance.instance[each.key].private_ip]
 }
 
 
 resource "null_resource" "ansible" {
-    depends_on = [
-      aws_instance.instance,
-      aws_route53_record.dns_records
+
+  depends_on = [
+    aws_instance.instance,
+    aws_route53_record.dns_records
+  ]
+
+
+  for_each      = var.components
+
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = aws_instance.instance[each.key].private_ip
+    }
+
+    inline = [
+      "ansible-pull -i localhost, -U https://github.com/sandeepkumar0088/roboshop-ansible-docker.git main.yml -e component=${each.key} -e env=dev"
     ]
 
-    for_each   = var.components
+  }
 
-    provisioner "remote-exec" {
-      connection {
-        type      = "ssh"
-        user      = "ec2-user"
-        password  = "DevOps321"
-        host      = aws_instance.instance[each.key].private_ip
-      }
-
-      inline = [
-        "ansible-pull -i localhost, -U https://github.com/Sandeepkumar0088/roboshop-ansible-docker.git main.yml -e component=${each.key} -e env=dev"
-      ]
-    }
 }
